5605f9c3a1a75c7a8b82727fb2e078e0acb5716f
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2026-05-06 21:20:43 +0700

    fix gradient descent

--- labwork1/gd.py
+++ labwork1/gd.py
@@ -6,13 +6,15 @@ def f_(x):
     return 2 * x
 
 
-def gd(x: float, lr: float, epoch: int):
-    for _ in range(epoch):
+def gd(x: float, lr: float, thres: float):
+    x = x - lr * f_(x)
+    while x >= thres:
         x = x - lr * f_(x)
-        print(f"{x} - {f(x)}")
+        print(f"x: {x} - f(x): {f(x)}")
+    print("Finished")
 
 
 if __name__ == "__main__":
     x = 10
     r = 0.1
-    gd(x, r, 4)
+    gd(x, r, 0.1)
